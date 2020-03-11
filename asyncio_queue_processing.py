561cc8176b531df954adcd5487a0cfeff620ee6e
Nathaniel Schaaf <schana@users.noreply.github.com>
2020-03-11 14:45:14 +0100

    Create asyncio_queue_processing.py

---
+++ asyncio_queue_processing.py
@@ -0,0 +1,36 @@
+import random
+import asyncio
+
+
+async def read_in(queue):
+    while True:
+        print(queue.qsize())
+        await queue.put(random.randint(0, 100))
+        #await asyncio.sleep(.001)
+
+
+async def process(in_q, out_q):
+    while True:
+        item = await in_q.get()
+        print('process', item)
+        await out_q.put(item)
+
+
+async def print_out(queue):
+    while True:
+        item = await queue.get()
+        print(item)
+
+
+def main():
+    in_q = asyncio.Queue(maxsize=100)
+    out_q = asyncio.Queue()
+    loop = asyncio.get_event_loop()
+    loop.create_task(read_in(in_q))
+    loop.create_task(process(in_q, out_q))
+    loop.create_task(print_out(out_q))
+    loop.run_forever()
+
+
+if __name__ == '__main__':
+    main()
